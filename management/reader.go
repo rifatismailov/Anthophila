72432ae6990c9c131d29c42707c90cedc8dc8a43
Rifat Ismailov <refaazer@gmail.com>
2024-08-28 13:04:37 +0300

    add manager.go

--- management/reader.go
+++ management/reader.go
@@ -10,13 +10,21 @@ import (
 	"strings"
 )
 
+// Reader - структура, яка забезпечує обробку отриманих повідомлень через WebSocket.
 type Reader struct{}
 
+// NewReader створює новий екземпляр `Reader`.
 func NewReader() *Reader {
 	return new(Reader)
 }
 
-// ReadMessage Обробка отриманих повідомлень
+// ReadMessage обробляє повідомлення, отримані через WebSocket.
+//
+// Параметри:
+// - ws: з'єднання WebSocket, з якого будуть читатися повідомлення.
+//
+// Опис:
+// Ця функція постійно читає повідомлення з WebSocket з'єднання. У разі помилки, функція логує її і припиняє обробку.
 func (r *Reader) ReadMessage(ws *websocket.Conn) {
 	for {
 		_, message, err := ws.ReadMessage()
@@ -28,6 +36,7 @@ func (r *Reader) ReadMessage(ws *websocket.Conn) {
 	}
 }
 
+// Command - структура для зберігання інформації про команду, отриману через WebSocket.
 type Command struct {
 	SClient string `json:"sClient"`
 	Command string `json:"command"`
@@ -35,15 +44,22 @@ type Command struct {
 
 var cmd Command
 
+// myMessage - структура для обміну повідомленнями між клієнтами через WebSocket.
 type myMessage struct {
 	SClient string `json:"sClient"`
 	RClient string `json:"rClient"`
 	Message string `json:"message"`
 }
 
-// ReadMessageCommand Обробка отриманих команд через WebSocket
+// ReadMessageCommand обробляє отримані команди через WebSocket.
+//
+// Параметри:
+// - wSocket: з'єднання WebSocket, з якого будуть читатися команди.
+//
+// Опис:
+// Ця функція обробляє команди, отримані через WebSocket. Вона підтримує команди, що потребують взаємодії з терміналом,
+// а також обробляє спеціальні команди, такі як `help`, `restart`, та `exit`.
 func (r *Reader) ReadMessageCommand(wSocket *websocket.Conn) {
-
 	term := terminal.NewTerminalManager()
 	if err := term.Start(); err != nil {
 		logging.Now().PrintLog("Failed to start terminal: ", err.Error())
@@ -95,15 +111,21 @@ func (r *Reader) ReadMessageCommand(wSocket *websocket.Conn) {
 					continue
 				}
 				term.SendCommand(cmd.Command)
-
 			}
 		}
 	}
 }
 
-// Terminal Запускає горутину для обробки виходу термінала
+// Terminal запускає горутину для обробки виходу термінала.
+//
+// Параметри:
+// - wSocket: з'єднання WebSocket, з якого буде відправлено повідомлення.
+// - term: екземпляр TerminalManager для керування терміналом.
+//
+// Опис:
+// Функція запускає горутину, яка читає вихідні дані з термінала і відправляє їх через WebSocket.
+// горутина буде працювати доти, доки є активний термінал, що видає вихідні дані, або доки не буде закрито саму програму.
 func Terminal(wSocket *websocket.Conn, term *terminal.TerminalManager) {
-	// Запускаємо горутину для обробки виходу термінала
 	go func() {
 		for line := range term.GetOutput() {
 			msg := myMessage{
